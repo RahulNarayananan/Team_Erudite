8e4c7cc8f3ec58e4be4c2e6f784988db1e62d89e
Nipunx23 <65994596+Nipunx23@users.noreply.github.com>
2022-01-22 22:05:24 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
 import youtube_dl
+import random
+from pytube import Playlist as pl
+# from pytube import YouTube as yt
+
+def ptol(url):
+  p = pl(url)
+  l = list(u for u in p.video_urls)
+  return l 
 
 class music(commands.Cog):
   def __intit__(self,client):
@@ -36,7 +44,79 @@ class music(commands.Cog):
       source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
       vc.play(source)
 
-  @commands.command()
+  @commands.command(aliases = ['meditate'])
+  async def med(self,ctx):
+    try:
+      ctx.voice_client.stop()
+    except:
+      pass
+    urls = ptol("https://www.youtube.com/playlist?list=PLw5NgVvYlQSWtIETvQeYTJrX3SYaDqCjt")
+    url = random.choice(urls)
+    FFMPEG_OPTIONS = {'before_options':'-reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url,download = False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+      vc.play(source)
+
+  @commands.command(aliases = ['smooth'])
+  async def sooth(self,ctx):
+    try:
+      ctx.voice_client.stop()
+    except:
+      pass
+    urls = ptol("https://www.youtube.com/playlist?list=PL6FB3Q57vUC-J2dJ77BOJiCBWtPlNH9F1")
+    url = random.choice(urls)
+    FFMPEG_OPTIONS = {'before_options':'-reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url,download = False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+      vc.play(source)
+
+  @commands.command(aliases = ['calming','anxiety','noanxiety'])
+  async def calm(self,ctx):
+    try:
+      ctx.voice_client.stop()
+    except:
+      pass
+    urls = ptol("https://www.youtube.com/playlist?list=PLt3RXjjdm-Rz-kyG4B009kHkZhUjBUfjG")
+    url = random.choice(urls)
+    FFMPEG_OPTIONS = {'before_options':'-reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url,download = False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+      vc.play(source)
+
+  @commands.command(aliases = ['happy','make_happy','happyme','happymood'])
+  async def hpy(self,ctx):
+    try:
+      ctx.voice_client.stop()
+    except:
+      pass
+    urls = ptol("https://www.youtube.com/playlist?list=PLucdVaCSKS8LVYxXgD1eIH6VZWh65mxYw")
+    url = random.choice(urls)
+    FFMPEG_OPTIONS = {'before_options':'-reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url,download = False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+      vc.play(source)
+
+  @commands.command(aliases = ['stop'])
   async def pause(self,ctx):
     ctx.voice_client.pause()
     await ctx.send("|PAUSED|")
